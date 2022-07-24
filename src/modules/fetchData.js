78d3077bbc19f5cc380394dca5e37093bf0e29f6
Mikhail <mikhailyu@hotmail.co.nz>
2022-07-24 15:03:43 +1200

    fixed github fetch issue

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -3,7 +3,8 @@ async function getWeather(location) {
     let response = await fetch(
       "http://api.openweathermap.org/data/2.5/weather?q=" +
         location +
-        "&APPID=01c00abe30f84eddc02f6860fd722dad&units=metric"
+        "&APPID=01c00abe30f84eddc02f6860fd722dad&units=metric",
+      { mode: "cors" }
     );
     let retrievedData = await response.json();
 
